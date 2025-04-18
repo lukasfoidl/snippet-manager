7d949ca1cf8c789f488cdca4c2b78681c6a81c03
Lukas Foidl <wi20b044@technikum-wien.at>
2025-04-18 21:10:27 +0200

    adapted regex

--- src/lib/utils/regex.ts
+++ src/lib/utils/regex.ts
@@ -1,7 +1,7 @@
-export const usernameRegex = /^[a-zA-Z0-9 öäüÖÄÜß#&\-]{1,20}$/; // escaping necessary otherwise error in client console
+export const usernameRegex = /^[a-zA-Z0-9 öäüÖÄÜß]{1,20}$/; // escaping necessary otherwise error in client console
 export const passwordRegex = /^.{8,100}$/;
 export const nameRegex = /^[a-zA-Z0-9 öäüÖÄÜß#&\-]{1,20}$/; // escaping necessary otherwise error in client console
 export const colorRegex = /#([0-9A-Fa-f]){6}/;
 export const titleRegex = /^[a-zA-Z0-9 öäüÖÄÜß#&\-]{1,20}$/; // escaping necessary otherwise error in client console
-export const descriptionRegex = /^[a-zA-Z0-9 öäüÖÄÜß#&\-]{0,50}$/; // escaping necessary otherwise error in client console
-export const contentRegex = /^[a-zA-Z0-9 öäüÖÄÜß#&\n\-]{1,1500}$/; // escaping necessary otherwise error in client console
+export const descriptionRegex = /^[a-zA-Z0-9 öäüÖÄÜß,;.:!?#&@€$%§/+*^()\-]{0,50}$/; // escaping necessary otherwise error in client console
+export const contentRegex = /^[a-zA-Z0-9 öäüÖÄÜß,;.:!?#&@€$%§/+*^()\n\-]{1,1500}$/; // escaping necessary otherwise error in client console
