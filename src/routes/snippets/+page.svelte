f6c4e313332e0a57d1f31e7dacd741b33e7135e7
Lukas Foidl <wi20b044@technikum-wien.at>
2025-04-18 20:48:50 +0200

    added snippet filter

--- src/routes/snippets/+page.svelte
+++ src/routes/snippets/+page.svelte
@@ -2,17 +2,62 @@
 	import MdiCopy from 'virtual:icons/mdi/content-copy';
 	import MdiSearch from 'virtual:icons/mdi/magnify';
 	import MdiFilter from 'virtual:icons/mdi/filter-remove';
+	import MdiClose from 'virtual:icons/mdi/close';
 	import CategorySelector from '$lib/components/categorySelector.svelte';
 	import FloatingPlusButton from '$lib/components/floatingPlusButton.svelte';
 	import CategoryBadge from '$lib/components/categoryBadge.svelte';
 	import NoDataFound from '$lib/components/noDataFound.svelte';
 	import { showToast } from '$lib/stores/toast';
+	import { derived, writable } from 'svelte/store';
+	import type { Snippet } from '$lib/types.js';
 
 	const { data } = $props();
 	let snippets = data.snippets || [];
 	let categories = data.categories || [];
 
-	let selectedCategoryIds = $state([]);
+	let selectedCategoryIds: number[] = $state([]);
+	const selectedCategoryIdsStore = writable<number[]>([]);
+
+	$effect(() => {
+		selectedCategoryIdsStore.set(selectedCategoryIds);
+	});
+
+	let search: string = $state('');
+	const searchStore = writable('');
+
+	const filteredSnippets = derived(
+		[writable(snippets), searchStore, selectedCategoryIdsStore],
+		([$snippets, $search, $selectedCategoryIds]) => {
+			const query = $search.toLowerCase();
+
+			return $snippets.filter((snippet: Snippet) => {
+				return (
+					(snippet.title.toLowerCase().includes(query) ||
+						snippet.description.toLowerCase().includes(query) ||
+						snippet.content.toLowerCase().includes(query)) &&
+					($selectedCategoryIds.length === 0 ||
+						selectedCategoryIds.every((id) =>
+							snippet.categories.some((category) => category.id === id)
+						))
+					// snippet.categories.some((category) => $selectedCategoryIds.includes(category.id)))
+				);
+			});
+		}
+	);
+
+	let timeout: ReturnType<typeof setTimeout>;
+
+	function setSearch() {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			searchStore.set(search);
+		}, 300);
+	}
+
+	function clearSearch() {
+		search = '';
+		searchStore.set('');
+	}
 
 	function copyToClipboard(text: string) {
 		navigator.clipboard
@@ -25,12 +70,20 @@
 				showToast('Failed copying to clipboard', 'error');
 			});
 	}
+
+	function resetFilter() {
+		clearSearch();
+		selectedCategoryIds = [];
+	}
 </script>
 
 <div class="mb-5 flex flex-row gap-1">
 	<label class="input focus-within:border-primary w-full focus-within:outline-0">
 		<MdiSearch class="h-5 w-5" />
-		<input type="input" placeholder="Search" />
+		<input type="input" placeholder="Search" bind:value={search} oninput={setSearch} />
+		<button class="btn btn-ghost h-7 w-7 p-0" onclick={clearSearch} title="Clear search">
+			<MdiClose class="h-5 w-5" />
+		</button>
 	</label>
 	<CategorySelector
 		{categories}
@@ -38,16 +91,16 @@
 		indicator={true}
 		title="Filter by category"
 	/>
-	<button class="btn btn-ghost p-2" title="Reset filter">
+	<button class="btn btn-ghost p-2" title="Reset filter" onclick={resetFilter}>
 		<MdiFilter class="h-5 w-5" />
 	</button>
 </div>
 
-{#if snippets.length === 0}
+{#if $filteredSnippets.length === 0}
 	<NoDataFound />
 {:else}
 	<ul class="list bg-base-100 rounded-box shadow-md">
-		{#each snippets as snippet (snippet.id)}
+		{#each $filteredSnippets as snippet (snippet.id)}
 			<li class="list-row hover:bg-base-300">
 				<div class="col-span-2 inline-flex justify-between">
 					<div>
