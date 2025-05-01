fae49f5531fae5e2ea49cc686fc670f3d656bcb9
Lukas Foidl <wi20b044@technikum-wien.at>
2025-05-01 09:25:02 +0200

    minor fixes

--- src/lib/components/noDataFound.svelte
+++ src/lib/components/noDataFound.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <div class="flex h-full flex-col items-center justify-center gap-2">
-	<MdiDatabaseOff class="text-primary h-20 w-20" />
-	<div class="text-primary">{$t('noDataFound')}</div>
+	<MdiDatabaseOff class="text-secondary h-20 w-20" />
+	<div class="text-secondary">{$t('noDataFound')}</div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -162,7 +162,7 @@
 
 		<footer class="footer footer-horizontal footer-center bg-secondary text-base-content gap-3 p-5">
 			<div class="grid grid-flow-col gap-4">
-				<a class="link link-hover" href="/imprint">Imprint</a>
+				<a class="link link-hover" href="/imprint">{$t('layout.imprint')}</a>
 			</div>
 			<div>
 				<p>© Lukas Foidl 2025 - All rights reserved</p>
